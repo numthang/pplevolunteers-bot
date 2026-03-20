fe39322626e70a66a4f8884248cd6046fd841528
Unnop Sricharoenchai <unnop.sricharoenchai@gmail.com>
2026-03-20 11:33:43 +0700

    User Rating/report

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 sudo -u www bash << 'EOF'
 cd /www/wwwroot/pplevolunteers-bot
 git pull
-node deploy-commands.js
-pm2 restart pplevolunteers-bot
+/www/server/nodejs/v24.14.0/bin/node deploy-commands.js
+/www/server/nodejs/v24.14.0/bin/pm2 restart pplevolunteers-bot
 echo "✅ Deploy เสร็จแล้ว"
 EOF
