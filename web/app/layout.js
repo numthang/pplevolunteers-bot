a0c2dcdeb7e866d897b7e0f26bde16f2f49e7062
Unnop Sricharoenchai <unnop.sricharoenchai@gmail.com>
2026-05-19 14:44:46 +0700

    Social shared improved

--- web/app/layout.js
+++ web/app/layout.js
@@ -9,7 +9,6 @@ export const metadata = {
     template: '%s — PPLE Volunteers',
   },
   description: 'PPLE Volunteers',
-  icons: { icon: '/logo.png' },
 }
 
 export default async function RootLayout({ children }) {
